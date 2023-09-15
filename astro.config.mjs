f3088b3bf751a03cb56cde48d604d69a286cf786
mayura-andrew <mayuraalahakoon@gmail.com>
2023-09-15 23:39:57 +0530

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
+import netlify from '@astrojs/netlify/edge-functions';
 
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:3000/',
-  output: 'hybrid',
+  output: 'server',
   experimental: {
     hybridOutput: true
   },
